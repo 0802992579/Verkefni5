0368763d42326667a4f25661e6287490e435227e
0802992579 <olafura@live.com>
2016-10-20 22:58:40 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="is">
   
   
@@ -19,24 +19,33 @@
 			shuffle($images);
 			$selectedImage1 = "images/{$images[0]['file']}.jpg";      
   			$caption1 = $images[0]['caption'];
+			$description1 = $images[0]['caption'];
 			$selectedImage2 = "images/{$images[1]['file']}.jpg";      
-  			$caption2 = $images[1]['caption']; ?>
-
+  			$caption2 = $images[1]['caption'];
+  			$description2 = $images[1]['caption']; ?>
     <section>
         <h2>A Thing for you</h2>
-        <p>A Thing has been very popular for three years now. It entered the market in 2013 and was immediately popular. A Thing is manifactured by The Thing Company, a company that specializes in making life easier for modern people.</p> 
-        <figure>        
-      		<img src="<?= $selectedImage1; ?>" alt="Random image">           
-   				<figcaption><?= $caption1; ?></figcaption>    
-      		</figure>
-		<figure>        
-      		<img src="<?= $selectedImage2; ?>" alt="Random image">           
-   				<figcaption><?= $caption2; ?></figcaption>    
+        <p>A Thing has been very popular for three years now. It entered the market in 2013 and was immediately popular. A Thing is manifactured by The Thing Company, a company that specializes in making life easier for modern people.</p>
+        <form method="GET" action="mynd.php">
+   
+        <figure>
+        <input type="hidden" name="caption" value="<?= $caption1; ?>" />
+        <input type="hidden" name="description" value="<?= $description1; ?>" />
+        <input type="hidden" name="image" value="<?= $selectedImage1; ?>" />
+        	<input type="image" name="submit" src="<?= $selectedImage1; ?>"  alt="Submit" width="100" height="100" />
+   			<figcaption><?= $caption1; ?></figcaption>    
       	</figure>
-
+		<figure>
+        <input type="hidden" name="caption" value="<?= $caption2; ?>" />
+        <input type="hidden" name="description" value="<?= $description2; ?>" />
+        <input type="hidden" name="image" value="<?= $selectedImage2; ?>" />
+      		<input type="image" name="submit" src="<?= $selectedImage2; ?>" alt="Submit" width="100" height="100" />          
+   			<figcaption><?= $caption2; ?></figcaption>    
+      	</figure>
+      	</form>
 	
 <p>A Thing has been described as the most usefull item since the ballpoint pen. Customers are universially happy with A Thing, buying more and more Things for teheir friends, family and fellow workers. Try it out - you will be amazed!</p> 
-        <p>A Thing is cheap enough for everyone and so the life millions of people can be improved. Make your life better and get A Thing to day. Buy A Thing today and join a evergrowing community of Thing-user. This close-knit community has been described by members as a second family where people find true support, comfort and warmth. Don´t leave it for an another day. Buy A Thing and join a ever growing community!</p>
+        <p>A Thing is cheap enough for everyone and so the life millions of people can be improved. Make your life better and get A Thing to day. Buy A Thing today and join a evergrowing community of Thing-user. This close-knit community has been described by members as a second family where people find true support, comfort and warmth. Don�t leave it for an another day. Buy A Thing and join a ever growing community!</p>
  		
         <p>A Thing has given rise to new usage of the word thing in english. You can read about it <a href="http://www.urbandictionary.com/define.php?term=A%20Thing">here</a> 
         and also  <a href="http://opinionator.blogs.nytimes.com/2016/04/16/is-that-even-a-thing/?_r=0">here</a>. We are extreme proud that your product has changed the English language and hope that in the future A Thing enter more languages such as Japanesese and Chineese</p>
