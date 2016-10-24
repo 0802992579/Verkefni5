38a6285be60a8ebbdcdefd7ec01439fa731eac0e
0802992579 <olafura@live.com>
2016-10-24 21:41:35 +0000

    Add files via upload

--- mynd.php
+++ mynd.php
@@ -2,31 +2,75 @@
     <title>A Thing<?php if (isset($title)) {echo "&#8212;{$title}";} ?> </title>
     <link rel="stylesheet" href="style/normalize.css">
     <link rel="stylesheet" href="style/stilsida.css">
+    <style>
+    .error {color: #FF0000;}
+     </style>
 </head>
-<form method="POST" action="page1.php">
+<body>
+<?php include 'process.php';?>
+
 <?php
-if($_GET['caption'] != "") {
-	$caption = $_GET['caption'];
-  echo"<input type='text' name='caption' value='" . $caption . "' /> <br>";
-} else {
-  echo"<input type='text' name='caption' value='missing' /> <br>";
+// define variables and set to empty values
+$send = true;
+$captionErr = $imageErr = $descriptionErr = $showErr = "";
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+$caption = $_GET["caption"];
+$image = $_GET["image"];
+$description = $_GET["description"];
 }
-if($_GET['image'] != "") {
-	$image = $_GET['image'];
-  echo"<input type='text' name='image' value='" . $image . "' /> <br>";
-} else {
-  echo"<input type='text' name='image' value='missing' /> <br>";
-}
-if($_GET['description'] != "") {
-	$description = $_GET['description'];
-  echo"<textarea rows='4' cols='50' name='description'>" . $description . "</textarea> <br>";
-} else {
-  echo"<textarea rows='4' cols='50' name='description'> missing </textarea> <br>";
+$show = "";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $send = true;
+  if (empty($_POST["caption"])) {
+    $captionErr = 'You need to fill out Caption';
+    $send = false;
+  } else {
+    $caption = clean($_POST["caption"]);
+  }
+  if (empty($_POST["image"])) {
+    $imageErr = 'You need to fill out Image';
+    $send = false;
+  } else {
+    $image = clean($_POST["image"]);
+  }
+  if (empty($_POST["description"])) {
+    $descriptionErr = 'You need to fill out Description';
+    $send = false;
+  } else {
+    $description = clean($_POST["description"]);
+  }
+  if (empty($_POST["show"])) {
+    $showErr = 'You need to select to show or not';
+    $send = false;
+  } else {
+    $show = clean($_POST["show"]);
+  }
 }
 ?>
-  <br>show
-  <input type="radio" name="birta" value="yes">YES
-  <input type="radio" name="birta" value="no">NO<br>
+
+<h1>Photo</h1>
+<p><span class="error">* required field.</span></p>
+<form method="POST" action="mynd1.php">
+
+Photo: <?php echo "<input type='text' name='caption' size='35' value='" . $caption . "' />"; ?>
+       <span class='error'>* <?php echo $captionErr;?></span>
+      <br><br>
+
+Link: <input type='text' name='image' size='35' value=' <?php echo $image;?> ' />
+      <span class='error'>* <?php echo $imageErr;?></span>
+      <br><br>
+
+Description: <textarea rows='4' cols='50' name='description'> <?php echo $description;?> </textarea>
+      <span class='error'>* <?php echo $descriptionErr;?></span>
+      <br><br>
+
+Show photo?
+  <input type="radio" name="show" value="yes">YES
+  <input type="radio" name="show" value="no">NO
+  <span class="error">* <?php echo $showErr;?></span>
   <br>
-  <input type="Submit" value="submit" />
+  <br>
+  <input type="Submit" value="Submit" />
 </form>
+
+</body>
